0d587a91ba7c8dcd5cf13a40042f47c86e820a93
pikesley <github@orgraphone.org>
2015-09-09 17:13:51 +0100

    Bad idea because revisions change

--- test/integration/production/serverspec/upstart_spec.rb
+++ test/integration/production/serverspec/upstart_spec.rb
@@ -18,17 +18,14 @@ describe file '/etc/init/services-manager-web-1.conf' do
   it { should be_file }
   its(:content) { should match /start on starting services-manager-web/ }
   its(:content) { should match /exec su - services-manager -c 'cd \/home\/services-manager\/services-manager.theodi.org\// }
-  its(:md5sum) { should eq '12b6e99f7c65321cc4cf615f44c7b13c' }
 end
 
 describe file '/etc/init/services-manager-scheduler-1.conf' do
   it { should be_file }
   its(:content) { should match /start on starting services-manager-scheduler/ }
-  its(:md5sum) { should eq 'bb27883840f509157868abdf1ced8253' }
 end
 
 describe file '/etc/init/services-manager-worker-4.conf' do
   it { should be_file }
   its(:content) { should match /start on starting services-manager-worker/ }
-  its(:md5sum) { should eq '37527e7fff53c8facc7e429863a9f81c' }
 end
