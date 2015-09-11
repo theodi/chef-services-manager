b84d06edd6e0482993a4fc76af2ed6c5ed7c1f52
pikesley <github@orgraphone.org>
2015-09-11 10:36:36 +0100

    Yeah, so we should get for this

--- test/integration/production/serverspec/env_spec.rb
+++ test/integration/production/serverspec/env_spec.rb
@@ -5,6 +5,6 @@ describe file '/home/services-manager/env' do
   it { should be_file }
 end
 
-#describe file '/home/services-manager/services-manager.theodi.org/current/.env' do
-#  it { should be_symlink }
-#end
+describe file '/home/services-manager/services-manager.theodi.org/current/.env' do
+  it { should be_symlink }
+end
