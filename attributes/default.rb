4258f35ec9a461cd24248607eb89070aee25a631
pikesley <github@orgraphone.org>
2015-09-30 16:49:59 +0100

    Shift these to Skellington

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,15 +7,6 @@ default['concurrency'] = 1
 default['ruby-ng']['ruby_version'] = '1.9.3'
 default['ruby-ng']['dev_package'] = '1.9.1'
 
-default['chef_client']['init_style'] = 'none'
-default['chef_client']['cron']['use_cron_d'] = true
-default['chef_client']['cron']['hour'] = '*'
-default['chef_client']['cron']['minute'] = '*/10'
-
-default['envbuilder']['base_dir'] = '/home/services-manager'
-default['envbuilder']['owner'] = 'services-manager'
-default['envbuilder']['group'] = 'services-manager'
-
 default['deployment']['revision'] = 'CURRENT'
 
 default['cert']['name'] = 'xero'
