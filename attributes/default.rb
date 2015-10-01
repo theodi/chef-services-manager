7c5941031ca32011b0b94794f70401520833537d
pikesley <github@orgraphone.org>
2015-10-01 16:42:54 +0100

    This is a hack
    This should be in a .foreman file per app

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['user'] = 'services-manager'
 default['fully_qualified_domain_name'] = 'services-manager.theodi.org'
 default['start_port'] = 3000
 default['repo'] = 'theodi/services-manager'
-default['concurrency'] = 1
+default['concurrency_string'] = 'worker=4,scheduler=1,web=1'
 
 default['ruby-ng']['ruby_version'] = '1.9.3'
 default['ruby-ng']['dev_package'] = '1.9.1'
