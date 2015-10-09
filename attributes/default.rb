059ee3efd38534810e640c8a60ceb30ff4a81310
pikesley <github@orgraphone.org>
2015-10-09 14:37:26 +0100

    Neaten things up

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 default['user'] = 'services-manager'
-default['fully_qualified_domain_name'] = 'services-manager.theodi.org'
+default['fully_qualified_domain_name'] = "#{node['user']}.theodi.org"
 default['start_port'] = 3000
-default['repo'] = 'theodi/services-manager'
-default['concurrency_string'] = 'worker=4,scheduler=1,web=1'
+default['repo'] = "theodi/#{node['user']}"
+default['concurrency'] = 1
+default['concurrency_string'] = "worker=4,scheduler=1,web=#{node['concurrency']}"
 
 default['ruby-ng']['ruby_version'] = '1.9.3'
-default['ruby-ng']['dev_package'] = '1.9.1'
 
 default['cert']['name'] = 'xero'
 default['cert']['file'] = 'privatekey.pem'
